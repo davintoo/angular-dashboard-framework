9772738d86f536355441e230604a4faf90b4c3d3
nazarazzUA <nazarazz@bigmir.net>
2014-11-13 17:07:58 +0200

    editMode

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -99,6 +99,7 @@ angular.module('adf')
       restrict: 'EA',
       transclude : false,
       scope: {
+      	editMode:'=',
         structure: '@',
         name: '@',
         collapsible: '@',
